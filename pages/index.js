b9fe09b5cfbb28676b88db446c66b8c4800f36e3
Gigin George <mail.gigin@gmail.com>
2024-06-12 04:15:05 +0530

    Fix Footer

--- pages/index.js
+++ pages/index.js
@@ -857,7 +857,7 @@ export default function Home() {
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500">
-              &copy; 2020 Your Company, Inc. All rights reserved.
+              &copy; 2024 Open Healthcare Network. All rights reserved.
             </p>
           </div>
         </div>
