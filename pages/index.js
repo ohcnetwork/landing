2d567fee6f529ab5af9cd36ad3a5b8beacb11c47
Bodhish Thomas <bodhish@gmail.com>
2024-06-12 19:00:51 +0530

    Add id for the section

--- pages/index.js
+++ pages/index.js
@@ -704,7 +704,7 @@ export default function Home() {
 
         {/* Our Deployments */}
 
-        <div className="overflow-hidden bg-white py-24">
+        <div id="deployments" className="overflow-hidden bg-white py-24">
           <div className="bg-white px-6 pb-10 sm:pb-16 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
