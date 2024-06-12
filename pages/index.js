941201a06c932cbe6a4863904a7ef202d610996b
Shivank Kacker <kacker.shivank@gmail.com>
2024-06-12 13:21:12 +0530

    added an image to assist content

--- pages/index.js
+++ pages/index.js
@@ -816,11 +816,14 @@ export default function Home() {
           </div>
         </div>
         <div className="text-black flex items-center justify-center py-20 bg-gray-50 my-20">
-          <div className="p-4 md:p-0 md:w-3/4 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-center">
+          <div className="p-4 md:p-0 md:w-3/4">
+          <div className="flex flex-col lg:flex-row items-center gap-8">
+<img src="/github-org-page.png" alt="Open Healthcare Network Github" className="w-full lg:w-[400px] rounded-lg" />
+<div className="">
+          <h1 className="text-4xl font-bold tracking-tight">
               Staying true to the &quot;Open&quot;
           </h1>
-          <p className="text-gray-500 text-center mt-6 text-sm">
+          <p className="text-gray-500 mt-6 text-sm">
             From the very beginning, Open Healthcare Network has embraced open-source principles, believing in the power of community and collaboration. Our mission is to foster a community of developers dedicated to leveraging technology to address healthcare challenges. By remaining open source, we ensure that innovation is driven by collective effort and shared knowledge. Join us in our journey to revolutionize healthcare through the power of open-source technology and community-driven solutions.          
           </p>
           <Link href={"https://github.com/coronasafe"} className="bg-primary-500 hover:bg-primary-600 text-white rounded-[100px] hover:rounded-2xl transition-all font-bold py-3 px-4 pr-4 inline-flex mt-6 items-center gap-2" >
@@ -829,10 +832,12 @@ export default function Home() {
           fillRule="evenodd"
           d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
           clipRule="evenodd"
-        />
+          />
       </svg> 
       Star us on Github
           </Link>
+          </div>
+          </div>
           </div>
         </div>
         <section
