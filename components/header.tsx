598a407c3647ada0051ba1722b6edd4a0745317c
Shivank Kacker <kacker.shivank@gmail.com>
2024-08-09 10:47:18 +0530

    forgot to push

--- components/header.tsx
+++ components/header.tsx
@@ -80,6 +80,7 @@ export default function Header(props: {
     const navigation: NavigationItem[] = [
         { type: "dropdown", content: "Products", items: productsItems },
         { type: "dropdown", content: "Community", items: communityItems },
+        { type: "link", content: "Supporters", href: "/supporters" },
         { type: "section", content: "Contact", id: "contact", page: "/" },
         { type: "link", content: <Image alt="Github" src="/logos/github-mark-white.svg" width={25} height={25} />, href: "https://github.com/coronasafe" }
     ];
@@ -238,41 +239,6 @@ export default function Header(props: {
                 <DropDownRender items={dropDownData || []} className="dropdown-animate-in" />
                 <DropDownRender items={outDropdownData || []} className="dropdown-animate-out" />
             </div>
-            <Dialog
-                className="lg:hidden"
-                open={mobileMenuOpen}
-                onClose={setMobileMenuOpen}
-            >
-                <div className="fixed inset-0 z-50" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-                    <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Open Healthcare Network</span>
-                            <Image
-                                src="/ohc_logo_white.png"
-                                alt=""
-                                width={200}
-                                height={40}
-                            />
-                        </a>
-                        <button
-                            type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-400"
-                            onClick={() => setMobileMenuOpen(false)}
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/25">
-                            <div className="space-y-2 py-6">
-                                {navigation.map((item, i) => <NavigationItemRender item={item} key={i} />)}
-                            </div>
-                        </div>
-                    </div>
-                </DialogPanel>
-            </Dialog>
         </header>
     )
 }
