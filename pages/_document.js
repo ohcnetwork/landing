ff79fbb91a6e80405224d2ac6190b1b3990b1cbb
rithviknishad <mail@rithviknishad.dev>
2024-10-22 10:33:00 +0530

    Add plausible scipt

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="ohc.network"
+          src="https://plausible.ohc.network/js/script.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
