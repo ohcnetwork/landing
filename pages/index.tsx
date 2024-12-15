d5bed66b6bab40dd61c9e86a0df2eb84877114a7
AdityaJ2305 <adityajindal2305@gmail.com>
2024-12-15 12:21:59 +0530

    console log to check

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,7 +149,7 @@ type Banner = {
 };
 export default function Home() {
   const router = useRouter();
-  console.log(router.pathname)
+  console.log(router.asPath)
   useEffect(() => {
     if (window.location.hash) {
       setTimeout(() => {
