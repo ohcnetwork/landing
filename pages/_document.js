b8492800438f5478bcd84c41f030e750f78bef2b
Aakash Singh <mail@singhaakash.dev>
2025-01-23 17:43:33 +0530

    Remove plausible

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          defer
-          data-domain="ohc.network"
-          src="https://plausible.ohc.network/js/script.js"
-        ></script>
-      </Head>
       <body>
         <Main />
         <NextScript />
