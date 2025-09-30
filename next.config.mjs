417d907acff4401e7e7b542e352899e5131bf8e5
Aayush6015 <153391642+Aayush6015@users.noreply.github.com>
2025-09-30 16:27:19 +0530

    Added X-frame-Options: DENY and Content-Security-Policy: frame-ancestors 'self' headers to next.config.mjs file. These headers prevent the site from being embedded in an iframe by external sites. Verified by creating a test HTML file that attempts to iframe the siteand the browser now successfully blocks the iframe as expected. (#80)

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // * - applies to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
