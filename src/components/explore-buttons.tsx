e0076f793bb16d434967afe90ae41fc731628231
nandkishorr <rachulr7@gmail.com>
2025-10-29 20:17:22 +0530

    fix:typo change in explore button

--- src/components/explore-buttons.tsx
+++ src/components/explore-buttons.tsx
@@ -5,7 +5,7 @@ export function ExploreButtons() {
          <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
                     <Button href="/contact">Request a demo</Button>
                     <Button variant="secondary" href="/developers">Deploy CARE </Button>
-                    <Button variant="secondary" href="/https://github.com/ohcnetwork">Explore the code</Button>
+                    <Button variant="secondary" href="https://github.com/ohcnetwork">Explore the code</Button>
                   </div>
     )
 }
